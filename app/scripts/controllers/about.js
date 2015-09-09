07b42cc7f3c58440774a80f6d74877a701270529
peter barraud <gapeterb@gmail.com>
2015-09-09 10:19:18 +0530

    your very first attempt at a yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schoolSiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the schoolSiteApp
+ */
+angular.module('schoolSiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schoolSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the schoolSiteApp
+ */
+angular.module('schoolSiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
